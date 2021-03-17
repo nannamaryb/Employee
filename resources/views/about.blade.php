0dd55a0c470481619435d038bb52e61456edd86a
nannamaryb <nannamarybernabas1234@gmail.com>
2021-03-17 15:46:27 +0530

    employee

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+@extends("theme")
+
+@section("content")
+
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse, beatae! Cum voluptatibus saepe nobis tempora commodi! Similique rerum ratione ad tenetur quidem, consequuntur vero eius sed repellendus dicta id ut.
+
+
+
+
+
+
+
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ Route::get('/', function () {
 Route::get('/search', function () {
     return view('search');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
